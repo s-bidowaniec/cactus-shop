3de6011e421f037674a9f6721d6ac52bcb8e486a
Stanislaw Bidowaniec <20791742+s-bidowaniec@users.noreply.github.com>
2023-01-21 18:54:55 +0100

    Add Header

--- client/src/components/views/Header/Header.js
+++ client/src/components/views/Header/Header.js
@@ -1,5 +1,31 @@
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from 'react-icons/fa';
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <Navbar sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to={'/'} as={NavLink}>Home</Nav.Link>
+        </Nav>
+        <Button style={{width: "3rem", height: "3rem", position: "relative"}} variant="outline-primary" className="rounded-circle">
+          <FaShoppingCart />
+          <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+               style={{
+                 color: "white",
+                 width: "1.5rem",
+                 height: "1.5rem",
+                 position:"absolute",
+                 bottom: 0,
+                 right: 0,
+                 transform: "translate(25%, 25%)"
+          }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </Navbar>);
 };
 
 export default Header;
